748995a1eae4619bc364872ccba6612bb025486d
Henri Karhatsu <henri@karhatsu.com>
2019-01-30 08:38:16 +0200

    Fetch and list tournaments

--- app/javascript/packs/components/main.jsx
+++ app/javascript/packs/components/main.jsx
@@ -2,18 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 export default class Main extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tournaments: undefined
+    }
+  }
+
   render() {
+    const { tournaments } = this.state
+    if (!tournaments) {
+      return <div>Loading...</div>
+    }
     return (
       <div>
-        <h1>fudisturnaus.com</h1>
+        {!tournaments.length ? 'Ei turnauksia' : tournaments.map(this.renderTournament)}
       </div>
     )
+  }
+
+  renderTournament = tournament => {
+    const { id, name, start_date } = tournament
+    return <div key={id}>{name}, {start_date}</div>
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/tournaments')
+      .then(response => response.json())
+      .then(tournaments => this.setState({ tournaments }))
+      .catch(console.error);
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Main/>,
-    document.body.appendChild(document.createElement('div')),
+    document.getElementById('react-app'),
   )
 })

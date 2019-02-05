dbb5f374070008e1a0565fb5c1b6ffb252a66215
Henri Karhatsu <henri@karhatsu.com>
2019-02-05 13:22:45 +0200

    Implement first version of result saving

---
+++ app/javascript/packs/components/official/group_stage_match.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class GroupStageMatch extends React.PureComponent {
+  static propTypes = {
+    accessKey: PropTypes.string.isRequired,
+    match: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      startTime: PropTypes.string.isRequired,
+      field: PropTypes.shape({
+        name: PropTypes.string.isRequired
+      }).isRequired,
+      homeTeam: PropTypes.shape({
+        name: PropTypes.string.isRequired
+      }).isRequired,
+      awayTeam: PropTypes.shape({
+        name: PropTypes.string.isRequired
+      }).isRequired,
+      homeGoals: PropTypes.number,
+      awayGoals: PropTypes.number,
+    }).isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+    const { match: { homeGoals, awayGoals } } = props
+    this.state = { resultOpen: false, homeGoals, awayGoals }
+  }
+
+  render() {
+    const { match: { startTime, field, homeTeam, awayTeam } } = this.props
+    return (
+      <tr>
+        <td>{startTime}</td>
+        <td>{field.name}</td>
+        <td>{homeTeam.name}</td>
+        <td>{awayTeam.name}</td>
+        <td>{this.renderResult()}</td>
+      </tr>
+    )
+  }
+
+  renderResult = () => {
+    const { resultOpen, homeGoals, awayGoals } = this.state
+    if (resultOpen) {
+      return (
+        <React.Fragment>
+          <input type="number" value={homeGoals || ''} onChange={this.setHomeGoals}/>
+          <span>-</span>
+          <input type="number" value={awayGoals || ''} onChange={this.setAwayGoals}/>
+          <input type="button" value="Tallenna" onClick={this.saveResult}/>
+        </React.Fragment>
+      )
+    } else {
+      return <a onClick={this.openResult}>{homeGoals} - {awayGoals}</a>
+    }
+  }
+
+  openResult = () => {
+    const { match: { homeGoals, awayGoals } } = this.props
+    this.setState({ resultOpen: true, homeGoals, awayGoals })
+  }
+
+  setHomeGoals = event => {
+    this.setState({ homeGoals: event.target.value })
+  }
+
+  setAwayGoals = event => {
+    this.setState({ awayGoals: event.target.value })
+  }
+
+  saveResult = () => {
+    const { accessKey, match: { id } } = this.props
+    const { homeGoals, awayGoals } = this.state
+    fetch(`/api/v1/official/group_stage_matches/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Key': accessKey
+      },
+      body: JSON.stringify({
+        group_stage_match: {
+          home_goals: homeGoals,
+          away_goals: awayGoals
+        }
+      })
+    })
+      .then(() => this.setState({ resultOpen: false }))
+      .catch(console.error) // eslint-disable-line no-console
+  }
+}
--- app/javascript/packs/components/official/official_main.jsx
+++ app/javascript/packs/components/official/official_main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import GroupStageMatch from './group_stage_match'
 
 export default class OfficialMain extends React.PureComponent {
   static propTypes = {
@@ -8,12 +9,51 @@ export default class OfficialMain extends React.PureComponent {
     tournamentId: PropTypes.number.isRequired
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      tournament: undefined
+    }
+  }
+
   render() {
+    const { tournament } = this.state
+    if (!tournament) {
+      return <div>Loading...</div>
+    }
     return (
       <div>
-        TODO
+        <h2>{tournament.name}</h2>
+        <h3>Alkusarjan ottelut</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Aika</th>
+              <th>Kenttä</th>
+              <th>Koti</th>
+              <th>Vieras</th>
+              <th>Tulos</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tournament.groupStageMatches.map(this.renderGroupStageMatch)}
+          </tbody>
+        </table>
       </div>
     )
+  }
+
+  renderGroupStageMatch = groupStageMatch => {
+    const { accessKey } = this.props
+    return <GroupStageMatch key={groupStageMatch.id} accessKey={accessKey} match={groupStageMatch} />
+  }
+
+  componentDidMount() {
+    const { tournamentId } = this.props
+    fetch(`/api/v1/tournaments/${tournamentId}`)
+      .then(response => response.json())
+      .then(tournament => this.setState({ tournament }))
+      .catch(console.error) // eslint-disable-line no-console
   }
 }
 

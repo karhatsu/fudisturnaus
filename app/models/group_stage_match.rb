48228e8acea11099c72de9a0cb3ddadfefd2e806
Henri Karhatsu <henri@karhatsu.com>
2019-02-06 17:04:09 +0200

    Validate that both goals or neither are saved

--- app/models/group_stage_match.rb
+++ app/models/group_stage_match.rb
@@ -6,10 +6,17 @@ class GroupStageMatch < ApplicationRecord
 
   validates :home_goals, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
   validates :away_goals, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  validate :verify_both_goals
 
   delegate :age_group_id, to: :group
 
   def tournament_id
     group.age_group.tournament_id
+  end
+
+  private
+
+  def verify_both_goals
+    errors.add :base, 'Molemmat maalit pitää syöttää' if !home_goals && away_goals || home_goals && !away_goals
   end
 end

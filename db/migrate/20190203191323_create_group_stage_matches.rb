3f1a12e7dae1ba6bdba81f90f2eff8ec3738f27d
Henri Karhatsu <henri@karhatsu.com>
2019-02-09 16:53:48 +0200

    Fix group_stage_matches migration

--- db/migrate/20190203191323_create_group_stage_matches.rb
+++ db/migrate/20190203191323_create_group_stage_matches.rb
@@ -4,8 +4,8 @@ class CreateGroupStageMatches < ActiveRecord::Migration[5.2]
       t.references :group, foreign_key: true, null: false
       t.references :field, foreign_key: true, null: false
       t.datetime :start_time, null: false
-      t.references :home_team, foreign_key: true, null: false
-      t.references :away_team, foreign_key: true, null: false
+      t.references :home_team, foreign_key: { to_table: :teams }, null: false
+      t.references :away_team, foreign_key: { to_table: :teams }, null: false
       t.integer :home_goals
       t.integer :away_goals
 

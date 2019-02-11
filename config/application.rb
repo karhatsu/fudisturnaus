28a306bfea21f42f2dbb7d1490f3836664bcfe5d
Henri Karhatsu <henri@karhatsu.com>
2019-02-11 14:34:16 +0200

    Start using domain fudisturnaus.com

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-DOMAIN = 'fudisturnaus.herokuapp.com'
+DOMAIN = 'fudisturnaus.com'
 
 module Fudisturnaus
   class Application < Rails::Application

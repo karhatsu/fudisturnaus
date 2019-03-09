1ee66e35d6f1a74107511ec9d55757fc555ba60e
Henri Karhatsu <henri@karhatsu.com>
2019-03-09 11:21:43 +0200

    Optimize tournament query

--- app/controllers/api/v1/tournaments_controller.rb
+++ app/controllers/api/v1/tournaments_controller.rb
@@ -6,7 +6,17 @@ class Api::V1::TournamentsController < ApplicationController
   end
 
   def show
-    includes = {age_groups: [groups: [group_stage_matches: [:field, :home_team, :away_team]]], groups: [teams: :club]}
+    includes = {
+      age_groups: [
+        playoff_matches: [:field, :home_team, :away_team],
+        groups: [
+          :age_group,
+          group_stage_matches: [:field, :home_team, :away_team]
+        ]
+      ],
+      fields: [],
+      groups: [teams: :club]
+    }
     @tournament = Tournament.where('id=?', params[:id]).includes(includes).first
   end
 end

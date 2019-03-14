3cec37eb9131b2fc1c4b65b1db12cdcc016aa64f
Henri Karhatsu <henri@karhatsu.com>
2019-03-14 06:58:00 +0200

    Fix club creation in official page

--- app/controllers/api/v1/official/official_base_controller.rb
+++ app/controllers/api/v1/official/official_base_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::Official::OfficialBaseController < ApplicationController
     official_access_key = request.headers['HTTP_X_ACCESS_KEY']
     admin_session_key = request.headers['HTTP_X_SESSION_KEY']
     if official_access_key
-      @tournament = Tournament.where('id=? AND access_key=?', tournament_id, official_access_key).first
+      @tournament = Tournament.where('access_key=?', official_access_key).first
       render status: 401, body: nil unless @tournament
     elsif admin_session_key
       ok = AdminSession.find_by_key admin_session_key

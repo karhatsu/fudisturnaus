c65a056da15ffc941c9ac452b29af19b364b1a6b
Henri Karhatsu <henri@karhatsu.com>
2020-10-04 10:05:54 +0300

    Show organizer logo in front page listing

--- app/controllers/api/v1/public/tournaments_controller.rb
+++ app/controllers/api/v1/public/tournaments_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Public::TournamentsController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def index
-    @tournaments = Tournament.all
+    @tournaments = Tournament.all.includes(:club)
   end
 
   def show

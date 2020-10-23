fae4bf2726d3e0d2dc90632c03f0245a8ab30663
Henri Karhatsu <henri@karhatsu.com>
2020-10-23 15:23:36 +0300

    Keep tournament order consistent in front page

--- app/controllers/api/v1/public/tournaments_controller.rb
+++ app/controllers/api/v1/public/tournaments_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Public::TournamentsController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def index
-    @tournaments = Tournament.all.includes(:club)
+    @tournaments = Tournament.all.includes(:club).except(:order).order('start_date DESC, name')
   end
 
   def show

2738c54938966392c2548564e759866d5635dc57
Henri Karhatsu <henri@karhatsu.com>
2020-10-24 15:07:52 +0300

    Redirect long tournament path to the shortened one

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,6 +2,7 @@ class TournamentsController < ApplicationController
   def show
     begin
       @tournament = Tournament.friendly.find(params[:id])
+      return redirect_to "/t/#{@tournament.slug}" if @tournament && params[:id].to_i.to_s == params[:id]
       render 'home/index'
     rescue ActiveRecord::RecordNotFound
       render 'home/not_found'

76ca7b46f1d6d4ee3098a0fa8d699e34f63a8916
Henri Karhatsu <henri@karhatsu.com>
2020-11-21 17:10:07 +0200

    Improve get-production-db.sh

--- get-production-db.sh
+++ get-production-db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-rm latest.dump
+rm -f latest.dump
 heroku pg:backups:download
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d fudisturnaus latest.dump

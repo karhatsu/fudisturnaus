f9e767f05e1c5ad543e72c2ddb765594cdc60e7e
Henri Karhatsu <henri@karhatsu.com>
2022-03-08 16:24:31 +0200

    Convert Index to functional

--- app/javascript/src/public/index.jsx
+++ app/javascript/src/public/index.jsx
@@ -3,15 +3,15 @@ import TournamentList from './tournament_list'
 
 const buildLink = tournament => `/t/${tournament.slug}`
 
-export default class Index extends React.PureComponent {
-  render() {
-    return (
-      <TournamentList
-        buildLink={buildLink}
-        showInfo={true}
-        showTestTournaments={false}
-        title="fudisturnaus.com"
-      />
-    )
-  }
+const Index = () => {
+  return (
+    <TournamentList
+      buildLink={buildLink}
+      showInfo={true}
+      showTestTournaments={false}
+      title="fudisturnaus.com"
+    />
+  )
 }
+
+export default Index

d197463fd9317f16ce13b5b8ce3fc8a5710ad688
Henri Karhatsu <henri@karhatsu.com>
2022-03-12 11:11:41 +0200

    Convert NewTournamentPage to functional

--- app/javascript/src/admin/new_tournament_page.jsx
+++ app/javascript/src/admin/new_tournament_page.jsx
@@ -1,60 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
+import React, { useContext, useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
 import TournamentFields from '../tournament_management/tournament_fields'
 import { createTournament, fetchClubs } from './api_client'
 import AccessContext from '../util/access_context'
 
-export default class NewTournamentPage extends React.PureComponent {
-  static propTypes = {
-    history: PropTypes.shape({
-      push: PropTypes.func.isRequired,
-    }).isRequired,
-  }
+const NewTournamentPage = () => {
+  const accessContext = useContext(AccessContext)
+  const [clubs, setClubs] = useState()
+  const history = useHistory()
 
-  static contextType = AccessContext
-
-  constructor(props) {
-    super(props)
-    this.state = { clubs: undefined }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="title">Uusi turnaus</div>
-        <div className="tournament-management__section">
-          <TournamentFields clubs={this.state.clubs} onCancel={this.goToIndex} onSave={this.onSave}/>
-        </div>
-      </div>
-    )
-  }
-
-  onSave = (data, callback) => {
-    createTournament(this.context, data, (errors, response) => {
-      if (errors) {
-        callback(errors)
-      } else {
-        this.goToTournamentPage(response.id)
-      }
-    })
-  }
-
-  goToTournamentPage = id => {
-    this.props.history.push(`/admin/tournaments/${id}`)
-  }
-
-  goToIndex = () => {
-    this.props.history.push('/admin')
-  }
-
-  componentDidMount() {
-    fetchClubs(this.context, (errors, response) => {
+  useEffect(() => {
+    fetchClubs(accessContext, (errors, response) => {
       if (errors) {
         console.error(errors)
       } else {
-        this.setState({ clubs: response.clubs })
+        setClubs(response.clubs)
+      }
+    })
+  }, [accessContext])
+
+  const onSave = (data, callback) => {
+    createTournament(accessContext, data, (errors, response) => {
+      if (errors) {
+        callback(errors)
+      } else {
+        goToTournamentPage(response.id)
       }
     })
   }
+
+  const goToTournamentPage = id => {
+    history.push(`/admin/tournaments/${id}`)
+  }
+
+  const goToIndex = () => {
+    history.push('/admin')
+  }
+
+  return (
+    <div>
+      <div className="title">Uusi turnaus</div>
+      <div className="tournament-management__section">
+        <TournamentFields clubs={clubs} onCancel={goToIndex} onSave={onSave}/>
+      </div>
+    </div>
+  )
 }
+
+export default NewTournamentPage

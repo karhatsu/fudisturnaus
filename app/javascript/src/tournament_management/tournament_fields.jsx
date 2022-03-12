425a2719238b15ccd758dad024022e7a44b4a517
Henri Karhatsu <henri@karhatsu.com>
2022-03-12 11:04:56 +0200

    Convert TournamentFields to functional

--- app/javascript/src/tournament_management/tournament_fields.jsx
+++ app/javascript/src/tournament_management/tournament_fields.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { formatTournamentDates } from '../util/date_util'
-import AccessContext from '../util/access_context'
 import FormErrors from '../form/form_errors'
 import TextField from '../form/text_field'
 import Button from '../form/button'
@@ -10,86 +9,54 @@ import VisibilityBadge from './visibility_badge'
 
 const { onlyTitle, teams, all } = visibilityTypes
 
-export default class TournamentFields extends React.PureComponent {
-  static propTypes = {
-    clubs: PropTypes.array,
-    onCancel: PropTypes.func,
-    onSave: PropTypes.func.isRequired,
-    tournament: PropTypes.shape({
-      cancelled: PropTypes.bool.isRequired,
-      clubId: PropTypes.number,
-      name: PropTypes.string.isRequired,
-      location: PropTypes.string.isRequired,
-      address: PropTypes.string,
-      startDate: PropTypes.string.isRequired,
-      endDate: PropTypes.string.isRequired,
-      days: PropTypes.number.isRequired,
-      matchMinutes: PropTypes.number.isRequired,
-      equalPointsRule: PropTypes.number.isRequired,
-      visibility: PropTypes.oneOf([onlyTitle, teams, all]).isRequired,
-    }),
-  }
+const initialData = {
+  cancelled: false,
+  clubId: undefined,
+  name: '',
+  startDate: '',
+  days: 1,
+  location: '',
+  address: '',
+  matchMinutes: 45,
+  equalPointsRule: 0,
+  visibility: teams,
+}
 
-  static contextType = AccessContext
+const TournamentFields = props => {
+  const { clubs, tournament, onCancel, onSave } = props
+  const [formOpen, setFormOpen] = useState(!tournament)
+  const [data, setData] = useState(initialData)
+  const [errors, setErrors] = useState([])
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      errors: [],
-      formOpen: !props.tournament,
-      form: {
-        cancelled: false,
-        clubId: undefined,
-        name: '',
-        startDate: '',
-        days: 1,
-        location: '',
-        address: '',
-        matchMinutes: 45,
-        equalPointsRule: 0,
-        visibility: teams,
-      },
-    }
-  }
-
-  render() {
-    const { formOpen } = this.state
-    if (formOpen) {
-      return this.renderTournamentForm()
-    } else {
-      return this.renderTournamentReadOnlyFields()
-    }
-  }
-
-  renderTournamentForm() {
+  const renderTournamentForm = () => {
     return (
       <form className="form form--vertical">
-        <FormErrors errors={this.state.errors}/>
-        {this.renderOrganizerField()}
-        {this.renderTournamentField('Nimi', 'text', 'name', 'Esim. Kevät Cup 2019')}
-        {this.renderTournamentField('Pvm', 'date', 'startDate')}
-        {this.renderTournamentField('Kesto (pv)', 'number', 'days')}
-        {this.renderTournamentField('Paikka', 'text', 'location', 'Esim. Kontulan tekonurmi')}
-        {this.renderTournamentField('Osoite', 'text', 'address', 'Esim. Tanhuantie 4-6, 00940 Helsinki')}
-        {this.renderTournamentField('Otteluiden välinen aika (min)', 'number', 'matchMinutes')}
-        {this.renderEqualPointsRuleField()}
-        {this.renderVisibilityField()}
-        {this.props.tournament && this.renderCheckbox('Peruttu', 'cancelled')}
-        {!this.props.tournament && this.renderCheckbox('Testiturnaus', 'test')}
-        {this.renderTournamentFormButtons()}
+        <FormErrors errors={errors}/>
+        {renderOrganizerField()}
+        {renderTournamentField('Nimi', 'text', 'name', 'Esim. Kevät Cup 2019')}
+        {renderTournamentField('Pvm', 'date', 'startDate')}
+        {renderTournamentField('Kesto (pv)', 'number', 'days')}
+        {renderTournamentField('Paikka', 'text', 'location', 'Esim. Kontulan tekonurmi')}
+        {renderTournamentField('Osoite', 'text', 'address', 'Esim. Tanhuantie 4-6, 00940 Helsinki')}
+        {renderTournamentField('Otteluiden välinen aika (min)', 'number', 'matchMinutes')}
+        {renderEqualPointsRuleField()}
+        {renderVisibilityField()}
+        {tournament && renderCheckbox('Peruttu', 'cancelled')}
+        {!tournament && renderCheckbox('Testiturnaus', 'test')}
+        {renderTournamentFormButtons()}
       </form>
     )
   }
 
-  renderOrganizerField() {
-    if (!this.props.clubs) return
+  const renderOrganizerField = () => {
+    if (!clubs) return
     return (
       <div className="form__field">
         <div className="label">Järjestävä seura</div>
         <div>
-          <select onChange={this.setValue('clubId')} value={this.state.form.clubId}>
+          <select onChange={setValue('clubId')} value={data.clubId}>
             <option>- Ei tiedossa -</option>
-            {this.props.clubs.filter(club => club.name.indexOf('Tuntematon') === -1).map(club => {
+            {clubs.filter(club => club.name.indexOf('Tuntematon') === -1).map(club => {
               return <option key={club.id} value={club.id}>{club.name}</option>
             })}
           </select>
@@ -98,16 +65,16 @@ export default class TournamentFields extends React.PureComponent {
     )
   }
 
-  renderTournamentField(label, type, field, placeholder) {
-    return <TextField label={label} onChange={this.setValue(field)} placeholder={placeholder} type={type} value={this.state.form[field]}/>
+  const renderTournamentField = (label, type, field, placeholder) => {
+    return <TextField label={label} onChange={setValue(field)} placeholder={placeholder} type={type} value={data[field]}/>
   }
 
-  renderEqualPointsRuleField() {
+  const renderEqualPointsRuleField = () => {
     return (
       <div className="form__field">
         <div className="label">Sääntö tasapisteissä</div>
         <div className="">
-          <select onChange={this.setValue('equalPointsRule')} value={this.state.form.equalPointsRule}>
+          <select onChange={setValue('equalPointsRule')} value={data.equalPointsRule}>
             <option value={0}>Kaikki ottelut (maaliero, tehdyt maalit), keskinäiset ottelut, arpa</option>
             <option value={1}>Keskinäiset ottelut, kaikki ottelut (maaliero, tehdyt maalit), arpa</option>
           </select>
@@ -116,12 +83,12 @@ export default class TournamentFields extends React.PureComponent {
     )
   }
 
-  renderVisibilityField() {
+  const renderVisibilityField = () => {
     return (
       <div className="form__field">
         <div className="label">Turnauksen näkyvyys</div>
         <div className="">
-          <select onChange={this.setValue('visibility')} value={this.state.form.visibility}>
+          <select onChange={setValue('visibility')} value={data.visibility}>
             <option value={onlyTitle}>Turnauksen perustiedot</option>
             <option value={teams}>Turnauksen perustiedot, sarjat ja joukkueet</option>
             <option value={all}>Turnauksen koko otteluohjelma</option>
@@ -131,34 +98,34 @@ export default class TournamentFields extends React.PureComponent {
     )
   }
 
-  renderCheckbox(label, field) {
+  const renderCheckbox = (label, field) => {
     return (
       <div className="form__field">
         <div className="label">{label}</div>
         <div>
-          <input type="checkbox" onChange={this.setCheckboxValue(field)} checked={this.state.form[field]} />
+          <input type="checkbox" onChange={setCheckboxValue(field)} checked={data[field]} />
         </div>
       </div>
     )
   }
 
-  renderTournamentFormButtons() {
+  const renderTournamentFormButtons = () => {
     return (
       <div className="form__buttons">
-        <Button label="Tallenna" onClick={this.submit} type="primary" disabled={!this.canSubmit()}/>
-        <Button label="Peruuta" onClick={this.cancel} type="normal"/>
+        <Button label="Tallenna" onClick={submit} type="primary" disabled={!canSubmit()}/>
+        <Button label="Peruuta" onClick={cancel} type="normal"/>
       </div>
     )
   }
 
-  renderTournamentReadOnlyFields() {
-    const { tournament: { name, startDate, endDate, location, address, visibility } } = this.props
+  const renderTournamentReadOnlyFields = () => {
+    const { name, startDate, endDate, location, address, visibility } = tournament
     const showBadge = visibility !== visibilityTypes.all
     const texts = [name, formatTournamentDates(startDate, endDate), location, address || '(ei osoitetta)']
     return (
       <div className="tournament-item">
         <div className="tournament-item__title tournament-item__title--existing" >
-          <span onClick={this.openForm} className={showBadge ? 'tournament-item__title--with-badge' : ''}>
+          <span onClick={openForm} className={showBadge ? 'tournament-item__title--with-badge' : ''}>
             <span>{texts.join(', ')}</span>
             {showBadge && <VisibilityBadge visibility={visibility}/>}
           </span>
@@ -167,45 +134,68 @@ export default class TournamentFields extends React.PureComponent {
     )
   }
 
-  openForm = () => {
-    const { tournament: { name, startDate, days, location, address, matchMinutes, equalPointsRule, visibility, clubId, cancelled } } = this.props
-    this.setState({
-      formOpen: true,
-      form: { name, startDate, days, location, address: address || '', matchMinutes, equalPointsRule, visibility, clubId, cancelled },
-    })
+  const resetForm = () => {
+    setFormOpen(false)
+    setErrors([])
   }
 
-  cancel = () => {
-    const { onCancel } = this.props
-    onCancel ? onCancel() : this.setState({ formOpen: false, errors: [] })
+  const openForm = () => {
+    const { name, startDate, days, location, address, matchMinutes, equalPointsRule, visibility, clubId, cancelled } = tournament
+    setData({ name, startDate, days, location, address: address || '', matchMinutes, equalPointsRule, visibility, clubId, cancelled })
+    setFormOpen(true)
   }
 
-  setValue = field => event => {
-    const { form } = this.state
-    this.setState({ form: { ...form, [field]: event.target.value } })
+  const cancel = () => {
+    onCancel ? onCancel() : resetForm()
   }
 
-  setCheckboxValue = field => event => {
-    const { form } = this.state
-    this.setState({ form: { ...form, [field]: event.target.checked } })
-  }
+  const setValue = field => event => setData({ ...data, [field]: event.target.value })
 
-  canSubmit = () => {
-    const { form: { days, name, startDate, location } } = this.state
+  const setCheckboxValue = field => event => setData({ ...data, [field]: event.target.checked })
+
+  const canSubmit = () => {
+    const { days, name, startDate, location } = data
     return parseInt(days) >= 1 && !!name && !!startDate && !!location
   }
 
-  submit = () => {
-    const { form } = this.state
-    form.name = form.name.trim()
-    form.location = form.location.trim()
-    form.address = form.address.trim()
-    this.props.onSave(form, errors => {
+  const submit = () => {
+    const trimmedData = { ...data }
+    trimmedData.name = trimmedData.name.trim()
+    trimmedData.location = trimmedData.location.trim()
+    trimmedData.address = trimmedData.address.trim()
+    onSave(trimmedData, errors => {
       if (errors) {
-        this.setState({ errors })
+        setErrors(errors)
       } else {
-        this.setState({ formOpen: false, errors: [] })
+        resetForm()
       }
     })
   }
+
+  if (formOpen) {
+    return renderTournamentForm()
+  } else {
+    return renderTournamentReadOnlyFields()
+  }
 }
+
+TournamentFields.propTypes = {
+  clubs: PropTypes.array,
+  onCancel: PropTypes.func,
+  onSave: PropTypes.func.isRequired,
+  tournament: PropTypes.shape({
+    cancelled: PropTypes.bool.isRequired,
+    clubId: PropTypes.number,
+    name: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    address: PropTypes.string,
+    startDate: PropTypes.string.isRequired,
+    endDate: PropTypes.string.isRequired,
+    days: PropTypes.number.isRequired,
+    matchMinutes: PropTypes.number.isRequired,
+    equalPointsRule: PropTypes.number.isRequired,
+    visibility: PropTypes.oneOf([onlyTitle, teams, all]).isRequired,
+  }),
+}
+
+export default TournamentFields

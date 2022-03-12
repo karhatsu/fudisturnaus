00b317ea8c4b53facccd6a69d4e00f1fc5987a22
Henri Karhatsu <henri@karhatsu.com>
2022-03-12 08:58:35 +0200

    Convert Team to functional

--- app/javascript/src/public/team.jsx
+++ app/javascript/src/public/team.jsx
@@ -1,40 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default class Team extends React.PureComponent {
-  static propTypes = {
-    clubId: PropTypes.number,
-    club: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      logoUrl: PropTypes.string,
-    }),
-    clubs: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      logoUrl: PropTypes.string,
-    })),
-    name: PropTypes.string.isRequired,
-    selected: PropTypes.bool,
-  }
+const Team = props => {
+  const { clubId, clubs, name, selected } = props
 
-  render() {
-    const { name } = this.props
-    const club = this.findClub()
-    return (
-      <span className={this.resolveClassNames()}>
-        {this.renderLogo(club)}
-        <span className="team__name">{name}</span>
-      </span>
-    )
-  }
+  const club = props.club ? props.club : clubs.find(c => c.id === clubId)
 
-  findClub = () => {
-    const { clubId, club, clubs } = this.props
-    if (club) return club
-    return clubs.find(c => c.id === clubId)
-  }
-
-  resolveClassNames = () => {
-    const { selected } = this.props
+  const resolveClassNames = () => {
     const classNames = ['team']
     if (selected) {
       classNames.push('team--selected')
@@ -42,9 +14,32 @@ export default class Team extends React.PureComponent {
     return classNames.join(' ')
   }
 
-  renderLogo(club) {
+  const renderLogo = () => {
     if (club && club.logoUrl) {
       return <img src={club.logoUrl} className="team__logo"/>
     }
   }
+
+  return (
+    <span className={resolveClassNames()}>
+      {renderLogo()}
+      <span className="team__name">{name}</span>
+    </span>
+  )
 }
+
+Team.propTypes = {
+  clubId: PropTypes.number,
+  club: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    logoUrl: PropTypes.string,
+  }),
+  clubs: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    logoUrl: PropTypes.string,
+  })),
+  name: PropTypes.string.isRequired,
+  selected: PropTypes.bool,
+}
+
+export default Team

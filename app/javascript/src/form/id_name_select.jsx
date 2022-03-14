3fdd1c6f70abbcd5174fe7afbf8a89be60bae5d6
Henri Karhatsu <henri@karhatsu.com>
2022-03-14 16:50:15 +0200

    Convert IdNameSelect to functional

--- app/javascript/src/form/id_name_select.jsx
+++ app/javascript/src/form/id_name_select.jsx
@@ -1,34 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default class IdNameSelect extends React.PureComponent {
-  static propTypes = {
-    customNameBuild: PropTypes.func,
-    field: PropTypes.string.isRequired,
-    formData: PropTypes.object.isRequired,
-    items: PropTypes.array.isRequired,
-    label: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-  }
-
-  render() {
-    const { field, formData, label, onChange } = this.props
-    return (
-      <div className="form__field">
-        <select onChange={onChange} value={formData[field]}>
-          <option>{label}</option>
-          {this.renderOptions()}
-        </select>
-      </div>
-    )
-  }
-
-  renderOptions() {
-    const { customNameBuild, items } = this.props
+const IdNameSelect = ({ customNameBuild, field, formData, items, label, onChange }) => {
+  const renderOptions = () => {
     const nameBuild = customNameBuild || (item => item.name)
     return items.map(item => {
       const { id } = item
       return <option key={id} value={id}>{nameBuild(item)}</option>
     })
   }
+
+  return (
+    <div className="form__field">
+      <select onChange={onChange} value={formData[field]}>
+        <option>{label}</option>
+        {renderOptions()}
+      </select>
+    </div>
+  )
 }
+
+IdNameSelect.propTypes = {
+  customNameBuild: PropTypes.func,
+  field: PropTypes.string.isRequired,
+  formData: PropTypes.object.isRequired,
+  items: PropTypes.array.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default IdNameSelect

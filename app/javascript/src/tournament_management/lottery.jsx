a184ba2a53b0c68bdfe1d1ad3f35c07f52a5070b
Henri Karhatsu <henri@karhatsu.com>
2022-03-14 08:38:43 +0200

    Convert Lottery to functional

--- app/javascript/src/tournament_management/lottery.jsx
+++ app/javascript/src/tournament_management/lottery.jsx
@@ -2,35 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import GroupLottery from './group_lottery'
 
-export default class Lottery extends React.PureComponent {
-  static propTypes = {
-    ageGroups: PropTypes.array.isRequired,
-    groups: PropTypes.arrayOf(PropTypes.shape({
-      ageGroupId: PropTypes.number.isRequired,
-      id: PropTypes.number.isRequired,
-      results: PropTypes.arrayOf(PropTypes.shape({
-        ranking: PropTypes.number.isRequired,
-        teamId: PropTypes.number.isRequired,
-        teamName: PropTypes.string.isRequired,
-        lot: PropTypes.number,
-      })),
-    })).isRequired,
-    onLotterySave: PropTypes.func.isRequired,
-    tournamentId: PropTypes.number.isRequired,
-  }
-
-  render() {
-    const { ageGroups, groups, onLotterySave, tournamentId } = this.props
-    const groupsWithResultsToBeSolved = groups.filter(group => group.results)
-    if (!groupsWithResultsToBeSolved.length) {
-      return this.renderInfo()
-    }
-    return groupsWithResultsToBeSolved.map(group => {
-      return <GroupLottery ageGroups={ageGroups} group={group} key={group.id} onLotterySave={onLotterySave} tournamentId={tournamentId}/>
-    })
-  }
-
-  renderInfo = () => {
+const Lottery = ({ ageGroups, groups, onLotterySave, tournamentId }) => {
+  const renderInfo = () => {
     return (
       <div className="tournament-item">
         Jos jossain lohkossa on tasapistetilanne, joka ei ratkea maalierolla tai keskinäisillä otteluilla,
@@ -38,4 +11,30 @@ export default class Lottery extends React.PureComponent {
       </div>
     )
   }
+
+  const groupsWithResultsToBeSolved = groups.filter(group => group.results)
+  if (!groupsWithResultsToBeSolved.length) {
+    return renderInfo()
+  }
+  return groupsWithResultsToBeSolved.map(group => {
+    return <GroupLottery ageGroups={ageGroups} group={group} key={group.id} onLotterySave={onLotterySave} tournamentId={tournamentId}/>
+  })
 }
+
+Lottery.propTypes = {
+  ageGroups: PropTypes.array.isRequired,
+  groups: PropTypes.arrayOf(PropTypes.shape({
+    ageGroupId: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+    results: PropTypes.arrayOf(PropTypes.shape({
+      ranking: PropTypes.number.isRequired,
+      teamId: PropTypes.number.isRequired,
+      teamName: PropTypes.string.isRequired,
+      lot: PropTypes.number,
+    })),
+  })).isRequired,
+  onLotterySave: PropTypes.func.isRequired,
+  tournamentId: PropTypes.number.isRequired,
+}
+
+export default Lottery

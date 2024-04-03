36d38294dd27481794c3390dad977d697ae0aea5
Henri Karhatsu <henri@karhatsu.com>
2024-04-03 13:22:08 +0300

    Fix contact email sending

--- app/controllers/api/v1/public/contacts_controller.rb
+++ app/controllers/api/v1/public/contacts_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Public::ContactsController < ApplicationController
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   def create
-    ContactMailer.contact_email(params[:name], params[:contact_info], params[:message],
+    ContactMailer.contact_email(params[:name], params[:email], params[:message],
                                 params[:tournament_organizer], params[:tournament_name],
                                 params[:tournament_start_date], params[:tournament_days],
                                 params[:tournament_location]).deliver_later
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
-  def contact_email(name, contact_info, message, tournament_organizer, tournament_name, tournament_start_date, tournament_days, tournament_location)
+  def contact_email(name, email, message, tournament_organizer, tournament_name, tournament_start_date, tournament_days, tournament_location)
     @name = name
-    @contact_info = contact_info
+    @email = email
     @message = message
     @tournament_organizer = tournament_organizer
     @tournament_name = tournament_name
@@ -9,7 +9,7 @@ class ContactMailer < ApplicationMailer
     @tournament_days = tournament_days
     @tournament_location = tournament_location
     system_email = ENV['EMAIL_ADDRESS']
-    reply_to = contact_info.index('@') ? contact_info : system_email
+    reply_to = email.index('@') ? email : system_email
     subject = @tournament_name.blank? ? 'yhteydenotto' : @tournament_name
     mail to: system_email, reply_to: reply_to, subject: "#{DOMAIN} - #{subject}"
   end

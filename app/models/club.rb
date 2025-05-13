f91d77d08ecaf26797e3c52995fec947f05357da
Henri Karhatsu <henri@karhatsu.com>
2025-05-13 08:57:12 +0300

    Add club.name_to_file_name method

--- app/models/club.rb
+++ app/models/club.rb
@@ -11,4 +11,8 @@ class Club < ApplicationRecord
   def self.by_name(name)
     self.find_by_name name
   end
+
+  def name_to_file_name
+    name.gsub(' ', '-').gsub(/[äåÄÅ]/, 'a').gsub(/[öÖ]/, 'o').downcase
+  end
 end
